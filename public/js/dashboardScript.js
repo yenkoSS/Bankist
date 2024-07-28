64b4c1709f78cf7ce4c86c0d9014903b24a25533
Stefan Petru-Cornel <petrucornel.stefan@gmail.com>
2024-07-28 15:59:24 +0300

    update donut chart

--- public/js/dashboardScript.js
+++ public/js/dashboardScript.js
@@ -1,14 +1,14 @@
 $(function () {
   'use strict';
   // ==============================================================
-  // Gender Js
+  // Incomes/Outcomes Raport Js
   // ==============================================================
 
   Morris.Donut({
-    element: 'gender_donut',
+    element: 'statistics_donut',
     data: [
-      { value: 60, label: 'Female' },
-      { value: 40, label: 'Male' },
+      { value: 60, label: 'Incomes' },
+      { value: 40, label: 'Outcomes' },
     ],
 
     labelColor: '#5969ff',
@@ -18,92 +18,41 @@ $(function () {
       return x + '%';
     },
   });
-
-  // ==============================================================
-  //  chart bar horizontal
-  // ==============================================================
-  var ctx = document.getElementById('chartjs_bar_horizontal').getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'horizontalBar',
-
-    data: {
-      labels: ['US', 'Brazil', 'Canada', 'UK', 'Australia', 'India', 'China'],
-      datasets: [
-        {
-          label: 'Country',
-          data: [2800, 24000, 19000, 17000, 14000, 10000, 7000],
-          backgroundColor: 'rgba(89, 105, 255, 1)',
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-      hover: false,
-      legend: {
-        display: true,
-        position: 'bottom',
-
-        labels: {
-          fontColor: '#71748d',
-          fontFamily: 'Circular Std Book',
-          fontSize: 14,
-        },
-      },
-      scales: {
-        legend: {
-          display: false,
-        },
-        yAxes: [
-          {
-            gridLines: {
-              drawOnChartArea: false,
-            },
-            ticks: {
-              fontSize: 14,
-              fontFamily: 'Circular Std Book',
-              fontColor: '#71748d',
-            },
-          },
-        ],
-        xAxes: [
-          {
-            gridLines: {
-              drawOnChartArea: false,
-            },
-            ticks: {
-              fontSize: 14,
-              fontFamily: 'Circular Std Book',
-              fontColor: '#71748d',
-            },
-          },
-        ],
-      },
-    },
-  });
 });
 
 async function fetchUser() {
   const cookie = document.cookie.split('=');
   const userId = cookie[1];
-  console.log(userId);
+
   const request = await fetch(`http://127.0.0.1:8000/api/v1/user/${userId}`);
   const response = await request.json();
-  console.log(response);
 
+  displayUserDetails(response.data);
   displayIncomes(calcIncomes(response.data));
   displayOutcomes(calcOutcomes(response.data));
   displayTotal(calcTotal(response.data));
-  displayUser(response.data.email);
+  displayTotalDebt(calcTotalDebt(response.data));
+  displayTransactions(response.data);
 }
 
 fetchUser();
+
+function displayUserDetails(value) {
+  const usernameEl = document.querySelector('.username');
+  const joinDateEl = document.querySelector('.joinDate');
+  const accountBadgeEl = document.querySelector('.accountBadge');
+
+  usernameEl.textContent = 'Welcome,' + ' ' + value.email.split('@')[0];
+  joinDateEl.textContent = 'Joining date:' + ' ' + value.joindate;
+  accountBadgeEl.textContent = value.accountType;
+}
 
 function calcIncomes(user) {
   const incomes = user.movements
     .map((mov) => mov.amount)
     .filter((amount) => amount > 0)
     .reduce((acc, curr) => acc + curr, 0);
-  return incomes;
+  return formatCurrency(incomes);
 }
 
 function calcOutcomes(user) {
@@ -111,14 +60,22 @@ function calcOutcomes(user) {
     .map((mov) => mov.amount)
     .filter((amount) => amount < 0)
     .reduce((acc, curr) => acc + curr, 0);
-  return Math.abs(outcomes);
+  return formatCurrency(Math.abs(outcomes));
 }
 
 function calcTotal(user) {
   const total = user.movements
     .map((mov) => mov.amount)
-    .reduce((acc, curr), acc + curr, 0);
-  return total;
+    .reduce((acc, curr) => acc + curr, 0);
+  return formatCurrency(total);
+}
+
+function calcTotalDebt(user) {
+  const totalDebt = user.movements
+    .filter((mov) => mov.transactionName === 'Credit')
+    .map((mov) => mov.amount)
+    .reduce((acc, curr) => acc + curr, 0);
+  return formatCurrency(totalDebt);
 }
 
 function displayIncomes(value) {
@@ -128,7 +85,7 @@ function displayIncomes(value) {
 
 function displayOutcomes(value) {
   const outcomesEl = document.querySelector('.outcomes');
-  outcomesEl = textContent = value;
+  outcomesEl.textContent = value;
 }
 
 function displayTotal(value) {
@@ -136,7 +93,33 @@ function displayTotal(value) {
   totalEl.textContent = value;
 }
 
-function displayUser(value) {
-  const usernameEl = document.querySelector('.username');
-  usernameEl.textContent = value.split('@')[0];
+function displayTotalDebt(value) {
+  const totalDebtEl = document.querySelector('.totalDebt');
+  totalDebtEl.textContent = value;
+}
+
+function formatCurrency(value) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(value);
+}
+
+function displayTransactions(transactions) {
+  const transactionsContainer = document.querySelector(
+    '.transactionsContainer'
+  );
+  transactions.movements.forEach((transaction) => {
+    const badge = transaction.amount > 0 ? 'income' : 'outcome';
+    const formatedAmount = formatCurrency(transaction.amount);
+    const html = `<tr>
+                    <td>
+                      <div class=${badge}></div>
+                    </td>
+                    <td>${transaction.transactionName}</td>
+                    <td class="transactionAmount">${formatedAmount}</td>
+                    <td class="transactionDate">${transaction.date}</td>
+                  </tr>`;
+    transactionsContainer.insertAdjacentHTML('afterbegin', html);
+  });
 }
