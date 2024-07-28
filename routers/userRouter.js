65fb98544f32172a164f6bfeda287b6574814c90
Stefan Petru-Cornel <petrucornel.stefan@gmail.com>
2024-07-28 16:00:22 +0300

    update user routers

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -13,10 +13,12 @@ router
 router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
 
-router.route('/').get(userController.getAllUsers);
-router.route('/').post(userController.postUser);
-router.route('/:id').get(userController.getUserById);
-router.route('/:id').put(userController.updateUserById);
-router.route('/:id').delete(userController.deleteUserById);
+router.route('/').get(authController.protect, userController.getAllUsers);
+router.route('/').post(authController.protect, userController.postUser);
+router.route('/:id').get(authController.protect, userController.getUserById);
+router.route('/:id').put(authController.protect, userController.updateUserById);
+router
+  .route('/:id')
+  .delete(authController.protect, userController.deleteUserById);
 
 module.exports = router;
